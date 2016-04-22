f7e2464c08e716ecdf3165b92c99fe90c12c9538
KDW <you@example.com>
2016-04-22 20:16:32 +0900

    initialize

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,34 @@
-// start slingin' some d3 here.
+var gameSetting = {
+  width: 700,
+  height: 450,
+  nEnemies: 30,
+  padding: 10
+};
 
+var gameScore = {
+  bestScore: 0,
+  now: 0
+};
+
+convertAxes = {
+  x: d3.scale.linear().domain([0, 100]).range([0, gameSetting.width]),
+  y: d3.scale.linear().domain([0, 100]).range([0, gameSetting.height])
+};
+
+
+function enemiesData(n) {
+  // holds json objects (for data)
+  var container = [];
+  for (var j = 0; j < n; j++) {
+    container[j] = {
+      "id": j,
+      "cx": convertAxes.x(Math.random() * 100),
+      "cy": convertAxes.y(Math.random() * 100)
+    };
+  }
+  return container;
+}
+
+d3.select('body').append('div').selectAll('p').data(enemiesData(5), function(d) {
+  return d.id;
+}).enter().append('p').text(function(d) {return "id: " +  d.id + " " + d.cx + " " + d.cy;});
