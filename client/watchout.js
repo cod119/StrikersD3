7f5662ad401a70adf25de8f13110ff5d589e0b57
Dongwoo Kim <idforcoding@gmail.com>
2016-05-18 04:48:15 +0900

    test

--- client/watchout.js
+++ client/watchout.js
@@ -1,15 +1,20 @@
 // global setting
+
 var gameSetting = {
-  width: 1000,
-  height: 800,
+  width:770,
+  height: 1000,
   nEnemies: 100,
   rEnemies: function() {return 5 + Math.random() * 10;},
   padding: 10,
-  rPlayer: 10,
+  rPlayer: 50,
   keySensitivity: 0,
-  rMissile : 3,
+  rMissile : 8,
   missileSpeedByPixel : 10,
-  enemyMissileNum: 3
+  //settings for enemy
+  enemyMissileNum: 5,
+  enemyTransitionDuration: 4999,
+  enemyMissileTransitionDuration: 8000,
+  enemyMissileFrequency: 6000
 };
 
 //gameSetting.nEnemies = prompt("Enter POSITIVE number of enemies, I said POSITIVE");
@@ -18,8 +23,11 @@ gameSetting.nEnemies = 10;
 var gameScore = {
   bestScore: 0,
   now: 0,
-  nCollisions: 0
+  nCollisions: 0,
+  nKilled: 0
 };
+
+
 
 // scales the range from 0 to 100 into gameboard's width and height
 var convertAxes = {
@@ -28,134 +36,120 @@ var convertAxes = {
 };
 
 // create data for enemies with randomized cx, cy
-function makeEnemiesData(n) {
-  // holds json objects (for data)
-  var container = [];
-  for (var j = 0; j < n; j++) {
-    container[j] = {
-      "id": j,
-      "cx": convertAxes.x(Math.random() * 100),
-      "cy": convertAxes.y(Math.random() * 100),
-      "r": gameSetting.rEnemies()
-    };
-  }
-  return container;
-}
+// function makeEnemiesData(n) {
+//   // holds json objects (for data)
+//   var container = [];
+//   for (var j = 0; j < n; j++) {
+//     container[j] = {
+//       "id": j,
+//       "cx": convertAxes.x(Math.random() * 100),
+//       "cy": convertAxes.y(Math.random() * 70),
+//       "r": gameSetting.rEnemies()
+//     };
+//   }
+//   return container;
+// }
 
 // creates a game board and enemies
-var canvas = d3.select("body").append("svg:svg")
+var canvas = d3.select("#map").append("svg:svg")
               .attr("width", gameSetting.width)
-              .attr("height", gameSetting.height);
-
-var enemies = function(data) {
-
-  var circles = canvas.selectAll(".enemy")
-                .data(data, function(d) { return d.id; });
-
-  circles.enter().append("circle")
-         .attr("cx", function(d) { return d.cx; })
-         .attr("cy", function(d) { return d.cy; })
-         .attr("r", function(d) { return d.r; })
-         .attr("class", "enemy");
-
-  circles.transition().ease("cubic-in-out").duration(1500)
-         .attr("cx", function(d) { return d.cx; })
-         .attr("cy", function(d) { return d.cy; });
-
-};
+              .attr("height", gameSetting.height)
+              .attr('transform','translate(0,0)');
 
 
 
+// var enemies = function(data) {
+//
+//   var circles = canvas.selectAll(".enemy")
+//                 .data(data, function(d) { return d.id; });
+//
+//   circles.enter().append("circle")
+//          .attr("cx", function(d) { return d.cx; })
+//          .attr("cy", function(d) { return d.cy; })
+//          .attr("r", function(d) { return d.r; })
+//          .attr("class", "enemy collide");
+//
+//   circles.transition().ease("cubic-in-out").duration(gameSetting.enemyTransitionDuration)
+//          .attr("cx", function(d) { return d.cx; })
+//          .attr("cy", function(d) { return d.cy; });
+//
+// };
 
-function enemyMissile(whose, num) {
-  this.whose = whose;
-  this.missileNum = num;
+
+var makeEnemies = function() {
   this.cnt = 0;
   this.data = [];
-  this.set = function() {
-    this.cnt += 1;
-    var cx = +this.whose.attr("cx");
-    var cy = +this.whose.attr("cy");
-    for (var i = 0; i < this.missileNum; i++) {
-      this.data.push({
-        id: this.cnt + "-" + i,
-        missileOrder: this.cnt,
-        identifier: i,
-        r: gameSetting.rMissile,
-        cx: cx,
-        cy: cy
-      });
-    }
-    var enemies = canvas.selectAll('enemyMissile')
-                  .data(this.data, function(d) { return d.id; })
-                  .enter().append('circle')
-                  .attr("cx", function(d) { return d.cx; })
-                  .attr("cy", function(d) { return d.cy; })
-                  .attr("r", function(d) { return d.r; })
-                  .attr("class", "enemyMissile enemy");
+  this.makeData = function() {
+    this.data.push({
+      "id": this.cnt,
+      "cx": convertAxes.x(Math.random() * 100),
+      "cy": convertAxes.y(Math.random() * 70),
+      "r": gameSetting.rEnemies()
+    });
+    this.cnt++;
   };
-  this.update = function() {
-    var speedByPixel = 100;
-    // console.log('data is', this.data);
-    for (var i = 0; i < this.data.length; i++) {
-      var pi = Math.PI * 2 / this.missileNum * this.data[i].identifier;
-      // console.log('pi is', pi);
-      this.data[i].cx += Math.cos(pi) * speedByPixel;
-      this.data[i].cy += Math.sin(pi) * speedByPixel;
-
-      console.log(Math.cos(pi), Math.sin(pi));
-      if (this.data[i].cx < 0 || this.data[i].cx > gameSetting.width || this.data[i].cy < 0 || this.data[i].cy > gameSetting.height) {
-        this.data.splice(i, 1);
-      }
-    }
-
-    var enemies = this.whose
+  this.render = function() {
+    var circles = canvas.selectAll(".enemy")
                   .data(this.data, function(d) { return d.id; });
 
-    enemies.attr("cx", function(d) { return d.cx; })
-          .attr("cy", function(d) { return d.cy; });
+    circles.enter().append("circle")
+           .attr("cx", function(d) { return d.cx; })
+           .attr("cy", function(d) { return d.cy; })
+           .attr("r", function(d) { return d.r; })
+           .attr("class", "enemy collide");
 
-    enemies.exit().remove();
+    circles.transition().ease("cubic-in-out").duration(gameSetting.enemyTransitionDuration)
+           .attr("cx", function(d) { return d.cx; })
+           .attr("cy", function(d) { return d.cy; });
   };
-}
-
-
-
-var enemyMissileAttach = function() {
-
-  var enemyMissiles = [];
-  // console.log(canvas.selectAll('.enemy'));
-  var enemies = canvas.selectAll('.enemy')
-                .each(function(d, i) {
-                  // console.log('hrer');
-                  // console.log(d3.select(this));
-                  enemyMissiles.push(new enemyMissile(d3.select(this), gameSetting.enemyMissileNum));
-                });
-  var set = function() {
-    // console.log('here');
-    // console.log(enemyMissiles);
-    for (var j = 0; j < enemyMissiles.length; j++) {
-
-      enemyMissiles[j].set();
-    }
-  };
-  var update = function() {
-    for (var k = 0; k < enemyMissiles.length; k++) {
-      console.log('k ', enemyMissiles[k]);
-      enemyMissiles[k].update();
-    }
-  };
-
-  return {
-    set: set,
-    update: update
+  this.collide = function(player) {
+    d3.selectAll(".enemy").each(function(d, i) {
+      if (collision(d3.select(this), player)) {
+        this.data.splice(+d3.select(this).attr("id"), 1);
+        var circles = canvas.selectAll(".enemy")
+                      .data(this.data, function(d) { return d.id; })
+                      .exit().remove();
+      }
+    });
   };
 };
 
 
+var enemyMissileFire = function(player) {
+  var playerX = player.attr('x') + player.attr('width') / 2;
+  var playerY = player.attr('y') + player.attr('height') / 2;
 
+  var enemies = canvas.selectAll('.enemy');
 
+  enemies.each(function(d, i) {
+    var thisX = +d3.select(this).attr('cx');
+    var thisY = +d3.select(this).attr('cy');
 
+    var num = gameSetting.enemyMissileNum;
+    for (var j = 0; j < num; j++) {
+      var pi = Math.PI * 2 / num * j;
+      // console.log('pi is', pi);
+      var criteriaLine = Math.sqrt(Math.pow(gameSetting.height, 2) + Math.pow(gameSetting.width, 2));
+      var missX = thisX + Math.cos(pi) * criteriaLine;
+      var missY = thisY + Math.sin(pi) * criteriaLine;
+
+      var missile = canvas.append('circle')
+                    .attr("cx", thisX).attr("cy", thisY)
+                    .attr("class", "enemyMissile collide")
+                    .transition().duration(gameSetting.enemyMissileTransitionDuration)
+                    .attr("cx", missX).attr("cy", missY).attr("r", gameSetting.rMissile);
+    }
+
+    var enemyMissile = canvas.selectAll('.enemyMissile')
+                      .each(function(d, i) {
+                        if (d3.select(this).attr("cx") < 0 || d3.select(this).attr("cx") > gameSetting.width || d3.select(this).attr("cy") < 0 || d3.select(this).attr("cy") > gameSetting.height) {
+                          d3.select(this).remove();
+                        }
+                      });
+
+  });
+
+};
 
 
 
@@ -166,9 +160,9 @@ var missile = function(whose) {
   this.data = [];
   this.set = function() {
     this.cnt += 1;
-    var cx = +this.whose.attr("cx");
-    var cy = +this.whose.attr("cy");
-    console.log('player position from missile : ', cx, cy);
+    var cx = +this.whose.attr("x") + player.attr('width') / 2;
+    var cy = +this.whose.attr("y") + player.attr('height') / 2;
+    // console.log('player position from missile : ', cx, cy);
     this.data.push({"id": this.cnt, "r": gameSetting.rMissile, "cx": cx, "cy": cy});
   };
   this.update = function() {
@@ -192,7 +186,7 @@ var missile = function(whose) {
 
 
     missiles.attr("r", function(d) { return d.r; })
-            .attr("id", function(d) { console.log("update");return d.id; })
+            .attr("id", function(d) { return d.id; })
             .attr("cx", function(d) { return d.cx; })
             .attr("cy", function(d) { return d.cy; });
 
@@ -265,35 +259,63 @@ var drag = d3.behavior.drag()
 
   // determines whether two circles have collided or not
   // accepts two d3 objects
-var collision = function(circle1, circle2) {
+function collision(circle1, circle2, option) {
   var result = false;
   // debugger;
-  var radiusTotal = +circle1.attr("r") + +circle2.attr("r");
-  // Using Pythogorean Theorem
-  // 피타고라스 정의 사용
-  var distance = Math.sqrt(Math.pow(+circle1.attr("cx") - +circle2.attr("cx"), 2) + Math.pow(+circle1.attr("cy") - +circle2.attr("cy"), 2));
-  // console.log(radiusTotal);
-  // console.log(distance);
+  if (option === undefined) {
+    var radiusTotal = +circle1.attr("r") + +circle2.attr("r");
+    // Using Pythogorean Theorem
+    // 피타고라스 정의 사용
+    var distance = Math.sqrt(Math.pow(+circle1.attr("cx") - +circle2.attr("cx"), 2) + Math.pow(+circle1.attr("cy") - +circle2.attr("cy"), 2));
+    // console.log(radiusTotal);
+    // console.log(distance);
+  } else if (option === 1) {
+    var radiusTotal = +circle1.attr("r") + Math.max(+circle2.attr("width"), +circle2.attr("height")) / 2;
+    // Using Pythogorean Theorem
+    // 피타고라스 정의 사용
+    var distance = Math.sqrt(Math.pow(+circle1.attr("cx") - +circle2.attr("x") - +circle2.attr("width") / 2, 2) + Math.pow(+circle1.attr("cy") - +circle2.attr("y") - +circle2.attr("height"), 2));
+    // console.log(radiusTotal);
+    // console.log(distance);
+  } else if (option = 2){
+    var radiusTotal = Math.max(+circle1.attr("width"), +circle1.attr("height")) / 2 + Math.max(+circle2.attr("width"), +circle2.attr("height")) / 2;
+    // Using Pythogorean Theorem
+    // 피타고라스 정의 사용
+    var distance = Math.sqrt(Math.pow(+circle1.attr("x") + +circle1.attr("width") / 2 - +circle2.attr("x") - +circle2.attr("width") / 2, 2) + Math.pow(+circle1.attr("y") + +circle1.attr("height") - +circle2.attr("y") - +circle2.attr("height"), 2));
+    // console.log(radiusTotal);
+    // console.log(distance);
+  }
+
   if (radiusTotal > distance) {
-    result = true;
+    result = [circle1.attr("id"), circle2.attr("id")];
   }
   return result;
 };
 
 // initial launch
 enemies(makeEnemiesData(gameSetting.nEnemies));
-
+// var explosion = canvas
+//   .append("svg:image")
+//   .attr("xlink:href", "static/explosion.gif")
+//   .attr("width", r)
+//   .attr("height", r)
+//   .attr("x", cx - r/2)
+//   .attr("y",cy - r/2);
 var player = canvas.selectAll(".player")
                    .data([{"id":"player","cx":gameSetting.width/2, "cy":gameSetting.height/2, "r":gameSetting.rPlayer}])
                    .enter()
-                   .append("circle").attr("class", "player")
-                   .attr("cx", function(d) {
+                   .append("svg:image").attr("class", "player")
+                   .attr("xlink:href", "static/spaceship.png")
+                   .attr("id", 1)
+                   .attr("x", function(d) {
                       return gameSetting.width / 2;
                    })
-                   .attr("cy", function(d) {
-                      return gameSetting.height / 2;
+                   .attr("y", function(d) {
+                      return gameSetting.height / 8 * 7;
                    })
-                   .attr("r", gameSetting.rPlayer).call(drag);
+                   .attr("width", gameSetting.rPlayer)
+                   .attr("height", gameSetting.rPlayer).call(drag);
+                  //  .append("svg:image")
+                  //  .attr("xlink:href", "static/spaceship.png")
 
 var player2 = canvas.selectAll(".player")
                   .data([{"id":"player","cx":gameSetting.width/2, "cy":gameSetting.height/2, "r":gameSetting.rPlayer}])
@@ -321,8 +343,8 @@ var move = function(unit, dir) {
     "2": {x: -speed, y: 0},
     "3": {x: speed, y: 0},
   };
-  var newcx = +unit.attr('cx') + d[dir].x;
-  var newcy = +unit.attr('cy') + d[dir].y;
+  var newcx = +unit.attr('x') + player.attr('width') / 2 + d[dir].x;
+  var newcy = +unit.attr('y') + player.attr('height') / 2 + d[dir].y;
 
   if (newcx > gameSetting.width - gameSetting.padding) {
     newcx = gameSetting.width - gameSetting.padding;
@@ -335,8 +357,8 @@ var move = function(unit, dir) {
     newcy = gameSetting.padding;
   }
   // console.log("direction is set");
-  unit.attr("cx", function(d) { return newcx; })
-        .attr("cy", function(d) { return newcy; });
+  unit.attr("x", function(d) { return newcx - player.attr('width') / 2; })
+        .attr("y", function(d) { return newcy - player.attr('height') / 2; });
 };
 var keyToMove = function(keyState) {
   for (var i = 0; i < 4; i++) {
@@ -356,7 +378,7 @@ var fire = function(unit, onfire) {
 };
 var keyToFire = function(keyState) {
   fire(playerWeapon, keyState[4]);
-  console.log('exe');
+  // console.log('exe');
 };
 
 
@@ -414,7 +436,7 @@ var keyboardUp = d3.select('body').on('keyup', function() {
 
 // executes moving according to keyState
 $(document).ready(function() {
-  var fireEnemyMissile = enemyMissileAttach();
+
 
   setInterval(function() {
     keyToMove(keyState);
@@ -428,16 +450,15 @@ $(document).ready(function() {
   }, 200);
 
   setInterval(function() {
-    fireEnemyMissile.set();
-    fireEnemyMissile.update();
-  }, 2000);
+    enemyMissileFire(player);
+  }, gameSetting.enemyMissileFrequency);
 
 
 
   // randomize enemies' position again
   setInterval(function() {
     enemies(makeEnemiesData(gameSetting.nEnemies));
-  }, 1500);
+  }, gameSetting.enemyTransitionDuration);
 
 
   // detects collision
@@ -445,8 +466,8 @@ $(document).ready(function() {
 
   setInterval(function() {
     var isCollide = false;
-    for (var i = 0; i < d3.selectAll('.enemy')[0].length; i++) {
-      isCollide = collision(d3.select(".player"), d3.select(d3.selectAll('.enemy')[0][i]));
+    for (var i = 0; i < d3.selectAll('.collide')[0].length; i++) {
+      isCollide = collision(d3.select(d3.selectAll('.collide')[0][i]), d3.select(".player"), 1);
       if (isCollide) {
         break;
       }
@@ -458,6 +479,22 @@ $(document).ready(function() {
 
       if (!isCollidedFlag) {
         gameScore.nCollisions++;
+        // var x = +player.attr("x");
+        // var y = +player.attr("y");
+        // var w = +player.attr("width");
+        // var h = +player.attr("height");
+        // var explosion = canvas
+        //   .append("svg:image")
+        //   .attr("xlink:href", "static/explosion.gif")
+        //   .attr("width", w)
+        //   .attr("height", h)
+        //   .attr("x", x - w/2)
+        //   .attr("y", y - h/2);
+        // setTimeout(function() {
+        //     explosion.remove();
+        // }, 1000);
+
+
         d3.select("svg").attr("class", "collision");
         isCollidedFlag = true;
       }
@@ -472,6 +509,36 @@ $(document).ready(function() {
                           .text(gameScore.now);
     d3.select(".collisions").select("span")
                           .text(gameScore.nCollisions);
+  }, 50);
+
+  setInterval(function() {
+    var enemy = d3.selectAll('.enemy');
+    enemy.each(function(d, i) {
+      var that = this;
+      d3.selectAll('.playerMissile').each(function(d, i) {
+        var ishit = collision(d3.select(that), d3.select(this));
+        var cx = d3.select(that).attr("cx");
+        var cy = d3.select(that).attr("cy");
+        var r = d3.select(that).attr("r") * 6;
+        if (ishit) {
+          gameScore.nKilled++;
+          d3.select(that).remove();
+          var explosion = canvas
+            .append("svg:image")
+            .attr("xlink:href", "static/explosion.gif")
+            .attr("width", r)
+            .attr("height", r)
+            .attr("x", cx - r/2)
+            .attr("y",cy - r/2);
+          d3.select(".killed").select("span")
+                                .text(gameScore.nKilled);
+          setTimeout(function() {
+              explosion.remove();
+          }, 1000);
+        }
+      });
+
+    });
   }, 50);
 
   // updates the current game score
